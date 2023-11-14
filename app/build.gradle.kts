635cb3139d7a9e8626ee0e715d89a1d7d10d02b6
JotautasMasys <mjotautas25@gmail.com>
2023-11-14 10:16:07 +0200

    Fourth commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.a4darbas"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a4darbas"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
